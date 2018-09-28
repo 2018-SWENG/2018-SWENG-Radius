5e63c9aca952e1ce727ced00d71cfd05394eca79
Oceanos1 <michael.spierer@heig-vd.ch>
2018-09-28 10:11:38 +0200

    added tests supports and changed the sdk target and sdk min to match bootcamp

---
+++ app/src/androidTest/java/ch/epfl/sweng/radius/MainActivityTest.java
@@ -0,0 +1,28 @@
+package ch.epfl.sweng.radius;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testCanGreetUsers() {
+        onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
+        onView(withId(R.id.mainButton)).perform(click());
+        // onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
+    }
+}
