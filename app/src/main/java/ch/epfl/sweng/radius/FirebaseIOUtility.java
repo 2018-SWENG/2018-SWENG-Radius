b424b98ad33fa9c084593634eb07498933eb773d
arthur <arthur.passuello@epfl.ch>
2018-10-09 17:45:42 +0200

    Save commit

--- app/src/main/java/ch/epfl/sweng/radius/FirebaseIOUtility.java
+++ app/src/main/java/ch/epfl/sweng/radius/FirebaseIOUtility.java
@@ -1,4 +1,27 @@
 package ch.epfl.sweng.radius;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 class FirebaseIOUtility {
+
+    private FirebaseDatabase database;
+
+    private DatabaseReference[] myRefs;
+
+    public FirebaseIOUtility(String [] references){
+
+        // Instanciate References Object
+        database    = FirebaseDatabase.getInstance();
+        myRefs      = new DatabaseReference[references.length];
+
+
+        // Fill fields reference array
+        for (int i = 0; i < references.length; i++){
+            DatabaseReference ref = database.getReference(references[i]);
+            myRefs[i] = ref;
+        }
+    }
 }
+
+
