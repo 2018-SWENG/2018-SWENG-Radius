57b9b30edb0f03457f3e9b11ee4d02e32b451848
arthur <arthur.passuello@epfl.ch>
2018-10-09 22:55:24 +0200

    Created test file

---
+++ app/src/test/java/ch/epfl/sweng/radius/FirebaseUtilityTest.java
@@ -0,0 +1,39 @@
+package ch.epfl.sweng.radius;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FirebaseUtilityTest {
+
+    @Test
+    public void checkNewUser() {
+    }
+
+    @Test
+    public void writeToDB() {
+    }
+
+    @Test
+    public void readFromDB() {
+    }
+
+    @Test
+    public void addStringListenerToDB() {
+    }
+
+    @Test
+    public void addIntListenerToDB() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
