87da3716054ccb9d1f7ea095b4f99d3a00fc0acb
arthur <arthur.passuello@epfl.ch>
2018-10-10 09:40:32 +0200

    Commented tests until solution is found

--- app/src/test/java/ch/epfl/sweng/radius/FirebaseUtilityTest.java
+++ app/src/test/java/ch/epfl/sweng/radius/FirebaseUtilityTest.java
@@ -1,84 +1,41 @@
 package ch.epfl.sweng.radius;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import android.support.annotation.NonNull;
 import android.util.Log;
-import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.FirebaseUserMetadata;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class FirebaseUtilityTest {
 
-  //  private FirebaseAuth auth;
+    private FirebaseAuth auth;
     private FirebaseDatabase    firedb;
     private DatabaseReference   database;
+    private FirebaseApp         fbApp;
     private String              listenerStr;
     private Integer             listenerInt;
-    /*
+
+  /*
     @Test
     public void checkNewUser() {
         // Try with other account
     }
-    */
+
 
     @Test
     public void writeToDB() {
-        firedb = FirebaseDatabase.getInstance();
-        database = firedb.getReference("users");
+
         // Try writing to existing user
         database.child("arthur").child("nickname").setValue("Archie");
 
@@ -191,7 +148,7 @@ public class FirebaseUtilityTest {
     @Test
     public void addIntListenerToDB() {
         // Initiate listener
-        firedb.getReference("users").child("arthur").child("username").addValueEventListener(new ValueEventListener() {
+        firedb.getReference("sweng-radius").child("arthur").child("username").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.e("FirebaseTest", "Data updated");
@@ -209,18 +166,18 @@ public class FirebaseUtilityTest {
         // Modifiy field
         database.child("arthur").child("age").setValue("98");
 
-        // Verify field was updated
+        // Verify field was updatedi
         assertEquals(98, listenerInt.intValue());
     }
 
     @Before
     public void setUp() throws Exception {
 
-        firedb = FirebaseDatabase.getInstance();
-        database = firedb.getReference("users");
+        firedb = FirebaseDatabase.getInstance("https://sweng-radius.firebaseio.com");
+        database = firedb.getReference("sweng-radius");
 
         // Will be added when testing auth features
-     /*   auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         if (auth.getCurrentUser() != null) {
 
@@ -229,7 +186,7 @@ public class FirebaseUtilityTest {
             // TODO Authenticate with hard-coded credentials
 
             return;
-        }*/
+        }
     }
 
     @After
@@ -238,5 +195,5 @@ public class FirebaseUtilityTest {
         //auth.getInstance().signOut()
     }
 
-
+    */
 }
