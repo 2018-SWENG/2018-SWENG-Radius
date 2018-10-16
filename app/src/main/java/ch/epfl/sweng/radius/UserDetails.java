14922547597d82d6a1f793748444c20d11d9a0ae
Oceanos1 <michael.spierer@heig-vd.ch>
2018-10-16 20:50:53 +0200

    MessageListActivityy  reworked to integrate firebase

--- app/src/main/java/ch/epfl/sweng/radius/UserDetails.java
+++ app/src/main/java/ch/epfl/sweng/radius/UserDetails.java
@@ -1,7 +1,7 @@
 package ch.epfl.sweng.radius;
 
 public class UserDetails {
-    static String username = "";
-    static String password = "";
-    static String chatWith = "";
+    public static String username = "";
+    public static String password = "";
+    public static String chatWith = "";
 }
--- app/src/main/java/ch/epfl/sweng/radius/message/MessageListActivity.java
+++ app/src/main/java/ch/epfl/sweng/radius/message/MessageListActivity.java
@@ -6,11 +6,17 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.epfl.sweng.radius.R;
+import ch.epfl.sweng.radius.UserDetails;
 import ch.epfl.sweng.radius.database.ChatLogs;
 import ch.epfl.sweng.radius.database.Message;
 import ch.epfl.sweng.radius.database.User;
@@ -23,11 +29,18 @@ import ch.epfl.sweng.radius.utils.UserInfos;
 public class MessageListActivity extends AppCompatActivity {
     private RecyclerView myMessageRecycler;
     private MessageListAdapter myMessageAdapter;
+    private EditText messageZone;
+    private Firebase reference1;
+    private Firebase reference2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_list);
+
+        messageZone = (EditText) findViewById(R.id.edittext_chatbox);
+
 
         // Test the chat view
         User alfred = new User();
@@ -37,10 +50,10 @@ public class MessageListActivity extends AppCompatActivity {
         participants.add(mika);
 
         Message m1 = new Message(1, alfred, "Hello", new Date());
-        Message m2 = new Message(2, mika,"Hello alfred", new Date());
-        Message m3 = new Message(3, alfred,"how are you ?", new Date());
+        Message m2 = new Message(2, mika, "Hello alfred", new Date());
+        Message m3 = new Message(3, alfred, "how are you ?", new Date());
 
-        ChatLogs messageList= new ChatLogs(participants);
+        ChatLogs messageList = new ChatLogs(participants);
         messageList.addMessage(m1);
         messageList.addMessage(m2);
         messageList.addMessage(m3);
@@ -61,11 +74,25 @@ public class MessageListActivity extends AppCompatActivity {
         myMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
         myMessageRecycler.setAdapter(myMessageAdapter);
 
+
+        Firebase.setAndroidContext(this);
+        reference1 = new Firebase("https://radius-1538126456577.firebaseio.com/messages/" + UserDetails.username + "_" + UserDetails.chatWith);
+        reference2 = new Firebase("https://radius-1538126456577.firebaseio.com/messages/" + UserDetails.chatWith + "_" + UserDetails.username);
+
+
         findViewById(R.id.button_chatbox_send).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //send
-                Log.println(Log.INFO, "info", "In the future, a message will be sent");
+                String message = messageZone.getText().toString();
+
+                if (!message.equals("")) {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", message);
+                    map.put("user", UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
+                    messageZone.setText("");
+                }
             }
         });
     }
