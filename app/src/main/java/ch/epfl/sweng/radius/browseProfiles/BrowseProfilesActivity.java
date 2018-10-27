07136492e69b674130aa8526ed8db1937d66cdf6
GodEmperorBerke <berke.egeli@ug.bilkent.edu.tr>
2018-10-27 18:55:27 +0200

    Added the report menu to the browse profile activity.

--- app/src/main/java/ch/epfl/sweng/radius/browseProfiles/BrowseProfilesActivity.java
+++ app/src/main/java/ch/epfl/sweng/radius/browseProfiles/BrowseProfilesActivity.java
@@ -3,8 +3,13 @@ package ch.epfl.sweng.radius.browseProfiles;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import ch.epfl.sweng.radius.R;
 
@@ -22,5 +27,26 @@ public class BrowseProfilesActivity extends AppCompatActivity {
         imageView.setImageResource(clickedPic);
         TextView textViewName = findViewById(R.id.clickedName);
         textViewName.setText(clickedName);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.spam:
+                Toast.makeText(this, "User is reported for spam.", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.language:
+                Toast.makeText(this, "User is reported for language.", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/ch/epfl/sweng/radius/utils/BrowseProfilesUtility.java
@@ -0,0 +1,4 @@
+package ch.epfl.sweng.radius.utils;
+
+public class BrowseProfilesUtility {
+}
