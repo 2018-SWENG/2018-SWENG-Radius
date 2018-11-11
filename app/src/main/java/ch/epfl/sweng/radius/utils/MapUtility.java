039ea1db3d6d4ae98ba0adaf8979d6c2dd0e0824
GodEmperorBerke <berke.egeli@ug.bilkent.edu.tr>
2018-11-11 13:31:51 +0100

    I don't even know what I have changed.

--- app/src/main/java/ch/epfl/sweng/radius/utils/MapUtility.java
+++ app/src/main/java/ch/epfl/sweng/radius/utils/MapUtility.java
@@ -125,7 +125,7 @@ public class MapUtility {
     }
 
     public boolean speaksSameLanguage(User user) {
-        String[] languagesSpoken = user.getProfileInfo().getSpokenLanguages().split(" ");
+        String[] languagesSpoken = user.getSpokenLanguages().split(" ");
         Fragment profileFragment = ProfileFragment.newInstance();
         String languagesSpokenByCurrUser = ((ProfileFragment)profileFragment).getLanguagesText();
 
