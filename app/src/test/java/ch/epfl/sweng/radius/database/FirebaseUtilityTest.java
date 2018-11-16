e42a40404217adb5db9f591ec3d493444ba7a0d9
arthur <arthur.passuello@epfl.ch>
2018-11-16 17:20:31 +0100

    Fix import error

--- app/src/test/java/ch/epfl/sweng/radius/database/FirebaseUtilityTest.java
+++ app/src/test/java/ch/epfl/sweng/radius/database/FirebaseUtilityTest.java
@@ -1,8 +1,11 @@
 package ch.epfl.sweng.radius.database;
 
+import android.support.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
