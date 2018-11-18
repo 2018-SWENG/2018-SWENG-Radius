0e9d461bc243c1785feada0378ffb719442d55ad
Oceanos1 <michael.spierer@heig-vd.ch>
2018-11-18 13:28:03 +0100

    added GroupTab.java

---
+++ app/src/main/java/ch/epfl/sweng/radius/home/GroupTab.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.radius.home;
+
+import java.util.List;
+
+import ch.epfl.sweng.radius.database.User;
+import ch.epfl.sweng.radius.utils.CustomLists.CustomTab;
+
+public class GroupTab extends CustomTab {
+
+    @Override
+    protected List<String> getUsersIds(User current_user) {
+        return null;
+    }
+}
