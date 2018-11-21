dcc70bbc1bb453b454c41690f77d5416b87466d4
arthur <arthur.passuello@epfl.ch>
2018-11-21 16:55:17 +0100

    Friends fragment implement DBIntfce

--- app/src/main/java/ch/epfl/sweng/radius/friends/FriendsTab.java
+++ app/src/main/java/ch/epfl/sweng/radius/friends/FriendsTab.java
@@ -2,14 +2,20 @@ package ch.epfl.sweng.radius.friends;
 
 import java.util.List;
 
+import ch.epfl.sweng.radius.database.DBObserver;
 import ch.epfl.sweng.radius.database.User;
 import ch.epfl.sweng.radius.utils.customLists.customUsers.CustomUserTab;
 
-public class FriendsTab extends CustomUserTab {
+public class FriendsTab extends CustomUserTab implements DBObserver {
     public FriendsTab(){ super();}
 
     protected List<String> getIds(User current_user){
         return current_user.getFriends();
+    }
+
+    @Override
+    public void onDataChange(String id) {
+
     }
 }
 
