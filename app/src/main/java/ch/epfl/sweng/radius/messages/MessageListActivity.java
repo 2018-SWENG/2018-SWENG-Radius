775504e9d406b8b6f2365cd85c5427dbb22c53bf
arthur <arthur.passuello@epfl.ch>
2018-11-22 12:01:48 +0100

    Fix refactoring error

--- app/src/main/java/ch/epfl/sweng/radius/messages/MessageListActivity.java
+++ app/src/main/java/ch/epfl/sweng/radius/messages/MessageListActivity.java
@@ -232,7 +232,7 @@ public class MessageListActivity extends AppCompatActivity implements DBObserver
            The problem here is, if the other user is in my radius I can send messages to them, however, if current user
            is not in the radius of the other user, they can not reply to those messages.*/
         if(chatLogs.getMembersId().size() == 2)
-            setEnabled(MapUtility.isInRadius(otherLoc, (int) MapUtility.radius));
+            setEnabled(MapUtility.isInRadius(otherLoc, (int) UserInfo.getInstance().getCurrentPosition().getRadius()));
         else
             setEnabled(true);
     }
