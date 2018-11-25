fd50f42cc89adef1bc03724fb2559b5e95985787
Efe Acer <efeacer@gmail.com>
2018-11-25 15:02:48 +0100

    Remove unnecessary code

--- app/src/main/java/ch/epfl/sweng/radius/home/TopicsTab.java
+++ app/src/main/java/ch/epfl/sweng/radius/home/TopicsTab.java
@@ -20,38 +20,12 @@ import ch.epfl.sweng.radius.utils.customLists.customUsers.CustomUserTab;
 
 public class TopicsTab extends CustomTopicTab implements DBLocationObserver {
 
-    private List<String> topicIDs = new ArrayList<>();
-
-    private CallBackDatabase locationsCallback = new CallBackDatabase() {
-        @Override
-        public void onFinish(Object value) {
-            ArrayList<MLocation> locations = (ArrayList<MLocation>) value;
-            for (MLocation location: locations){
-                if (checkLocationConditions(location)) {
-                    topicIDs.add(location.getID());
-                }
-            }
-        }
-
-        @Override
-        public void onError(DatabaseError error) {
-            Log.e("TopicsTab", "Database read error all locations");
-        }
-    };
-
-    private boolean checkLocationConditions(MLocation location) {
-        return MapUtility.isInRadius(location) && location.isVisible() &&
-                location.getLocationType() == 0;
-    }
-
     public TopicsTab() {
         OthersInfo.getInstance().addLocationObserver(this);
     }
 
     protected  List<String> getIds(User current_user) {
-        Database.getInstance().readAllTableOnce(Database.Tables.LOCATIONS, locationsCallback);
-        List<String> result = new ArrayList<>(OthersInfo.getInstance().getTopicsPos().keySet());
-        return result;
+        return new ArrayList<>(OthersInfo.getInstance().getTopicsPos().keySet());
     }
 
     @Override
