8d805059728e8d55e5703dc6b75f0e3a64283ebe
arthur <arthur.passuello@epfl.ch>
2018-12-02 19:51:55 +0100

    Save+

--- app/src/main/java/ch/epfl/sweng/radius/utils/NotificationUtility.java
+++ app/src/main/java/ch/epfl/sweng/radius/utils/NotificationUtility.java
@@ -1,21 +1,27 @@
 package ch.epfl.sweng.radius.utils;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 
 import ch.epfl.sweng.radius.R;
 
-public class NotificationUtility {
+public class NotificationUtility{
 
     private static int unseenMsg = 0;
     private static int unseenReq = 0;
     private Context context;
     private String channelID;
+    private NotificationManager notificationManager;
 
-    public NotificationUtility(Context context, String chanelID){
+    public NotificationUtility(Context context, String chanelID, NotificationManager notificationManager){
         this.context = context;
         this.channelID = chanelID;
+        this.notificationManager = notificationManager;
 
 
     }
