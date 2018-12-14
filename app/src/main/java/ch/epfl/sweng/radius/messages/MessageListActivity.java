8d18b812b21564b19c9532bab7a56cd9f37cb9f1
arthur <arthur.passuello@epfl.ch>
2018-12-14 08:13:02 +0100

    Fix CC issue

--- app/src/main/java/ch/epfl/sweng/radius/messages/MessageListActivity.java
+++ app/src/main/java/ch/epfl/sweng/radius/messages/MessageListActivity.java
@@ -209,19 +209,16 @@ public class MessageListActivity extends AppCompatActivity implements DBLocation
     private void compareLocation() {
         //TODO check if other users radius contains current user.
        // Log.e("RealTimeDebug", "User is in table : " + String.valueOf(OthersInfo.getInstance().getUsersInRadius().containsKey(otherUserId)) + otherUserId);
-        if(chatLogs.getMembersId().size() != 2 ) {
+        if(chatLogs.getMembersId().size() != 2 || locType != 0) {
             if(!isEnabled){ toggleFlagAndSendingFields(true);}
             return;
         }
          if(!OthersInfo.getInstance().getUsersInRadius().containsKey(otherUserId)){
              toggleFlagAndSendingFields(false); }
-         else{
-             if (locType == 0) handleUserChat();
+         else handleUserChat();
                //  Log.e("RealTimeDebug", "User is visible : " + String.valueOf(OthersInfo.getInstance().getUsersInRadius().get(otherUserId).isVisible()));
                //  Log.e("RealTimeDebug", "Chat is enabled: " + isEnabled);
-             else setEnabled(true);
-
-         }
+         
 
             }
 
