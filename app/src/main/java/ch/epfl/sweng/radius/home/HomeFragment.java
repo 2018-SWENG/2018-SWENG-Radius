18382bde1cc6c3b57ccb92615dc2fdc5cc4f3802
Pinar Ayaz <pinarayaz123@gmail.com>
2018-12-16 20:27:07 +0100

    Codeclimate issues

--- app/src/main/java/ch/epfl/sweng/radius/home/HomeFragment.java
+++ app/src/main/java/ch/epfl/sweng/radius/home/HomeFragment.java
@@ -82,8 +82,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, DBLoca
     }*/
 
     // For debug purpose only
-    public static HomeFragment newInstance(MapUtility mapUtility, GoogleMap googleMap,
-                                           int radiusValue) {
+    public static HomeFragment newInstance(MapUtility mapUtility, GoogleMap googleMap, int radiusValue){
         HomeFragment fragment = new HomeFragment();
         radius = radiusValue;
         mobileMap = googleMap;
@@ -276,24 +275,19 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, DBLoca
         getFriendsID();
         if(usersLoc != null) {
             for (int i = 0; i < usersLoc.size(); i++) {
-                markNearbyUser(i, usersLoc.get(i).getMessage(), usersLoc.get(i).getTitle(),
-                usersLoc.get(i).getID());
+                markNearbyUser(i, usersLoc.get(i).getMessage(), usersLoc.get(i).getTitle(), usersLoc.get(i).getID());
             }
         }
     }
 
     public void getFriendsID() {
-
         friendsID = UserInfo.getInstance().getCurrentUser().getFriends();
-
     }
 
     public void markNearbyUser(int indexOfUser, String status, String userName, String locID) {
         if(!usersLoc.get(indexOfUser).isVisible()) return;
-        LatLng newPos = new LatLng(usersLoc.get(indexOfUser).getLatitude(),
-                                    usersLoc.get(indexOfUser).getLongitude()    );
-        float color = friendsID.containsKey(locID) ? BitmapDescriptorFactory.HUE_BLUE :
-                                                        BitmapDescriptorFactory.HUE_RED;
+        LatLng newPos = new LatLng(usersLoc.get(indexOfUser).getLatitude(), usersLoc.get(indexOfUser).getLongitude());
+        float color = friendsID.containsKey(locID) ? BitmapDescriptorFactory.HUE_BLUE : BitmapDescriptorFactory.HUE_RED;
 
         //Change the marker color to green if users speak the same language but they are not friends
         if(color != BitmapDescriptorFactory.HUE_BLUE){
@@ -320,11 +314,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, DBLoca
             public void run(){
                 if(mobileMap != null && mobileMap.getProjection() != null)
                     mobileMap.addMarker(marker);
-
             }
             });
         }catch(NullPointerException e){/* Only happens in Unit Test*/}
-
 
     }
 
@@ -333,8 +325,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, DBLoca
 
         radius = UserInfo.getInstance().getCurrentPosition().getRadius();
         Log.e("OnLocationChange", "radius : " + radius);
-        coord = new LatLng(UserInfo.getInstance().getCurrentPosition().getLatitude(),
-                UserInfo.getInstance().getCurrentPosition().getLongitude());
+        coord = new LatLng(UserInfo.getInstance().getCurrentPosition().getLatitude(), UserInfo.getInstance().getCurrentPosition().getLongitude());
         if (getActivity() != null && !Database.DEBUG_MODE) {
             initCircle(coord);
             markNearbyUsers();
