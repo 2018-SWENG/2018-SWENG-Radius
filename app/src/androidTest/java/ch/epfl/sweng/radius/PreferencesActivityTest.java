ccb180c3ed100395cb51ed6f41b0dbcbadd97755
arthur <arthur.passuello@epfl.ch>
2018-12-17 07:12:59 +0100

    Try to fix coverage

--- app/src/androidTest/java/ch/epfl/sweng/radius/PreferencesActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/radius/PreferencesActivityTest.java
@@ -6,6 +6,7 @@ import android.preference.Preference;
 import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.FrameLayout;
 
@@ -15,8 +16,13 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 
 import ch.epfl.sweng.radius.database.Database;
+import ch.epfl.sweng.radius.database.MLocation;
+import ch.epfl.sweng.radius.database.User;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -74,11 +80,7 @@ public class PreferencesActivityTest  extends ActivityInstrumentationTestCase2<P
 
     @Test
     public void testIncognitoMode(){
-        try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         Espresso.onView(AllOf.allOf(withText(R.string.incognitoTitle)))
                 .perform(click());
     }
@@ -105,15 +107,49 @@ public class PreferencesActivityTest  extends ActivityInstrumentationTestCase2<P
     public void testDeleteAccountDismiss(){
         Espresso.onView(AllOf.allOf(withText(R.string.deleteAccountTitle)))
                 .perform(click());
-        Espresso.onView(withText("Dismiss"));
+        Espresso.onView(withText("Dismiss")).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    @Ignore
     public void testDeleteAccount(){
         Espresso.onView(AllOf.allOf(withText(R.string.deleteAccountTitle)))
                 .perform(click());
-        Espresso.onView(withText("Delete"));
+        Espresso.onView(withText("Delete")).perform(click());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        restoreCurrentUser();
+    }
+
+    private void restoreCurrentUser() {
+
+        User currentUSer = new User("testUser1");
+        currentUSer.addChat("testUser2", "10");
+        currentUSer.addChat("testUser3", "11");
+        currentUSer.addChat("testUser5", "12");
+        currentUSer.addFriendRequest(new User("testUser5"));
+        ArrayList<String> blockedUser = new ArrayList<>();
+        blockedUser.add("testUser3");currentUSer.setBlockedUsers(blockedUser);
+
+        currentUSer.addFriendRequest(new User("testUser3"));
+
+        Database.getInstance().writeInstanceObj(currentUSer, Database.Tables.USERS);
+
+        MLocation currentLoc = new MLocation("testUser1");
+        currentLoc.setUrlProfilePhoto("./app/src/androidTest/java/ch/epfl/sweng/radius/utils/default.png");
+        currentLoc.setTitle("testUser1");
+        currentLoc.setRadius(30000); currentLoc.setMessage("Being tested on");
+        currentLoc.setInterests("Tests, mostly");
+
+        Database.getInstance().writeInstanceObj(currentLoc, Database.Tables.LOCATIONS);
+
     }
 
     @After
