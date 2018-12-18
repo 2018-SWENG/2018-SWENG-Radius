77ea9cddff2ed95b15666bbece69e1853d9b9439
arthur <arthur.passuello@epfl.ch>
2018-12-18 20:29:47 +0100

    Improve coverage

--- app/src/test/java/ch/epfl/sweng/radius/PreferencesActivityTest.java
+++ app/src/test/java/ch/epfl/sweng/radius/PreferencesActivityTest.java
@@ -1,17 +1,51 @@
 package ch.epfl.sweng.radius;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import ch.epfl.sweng.radius.database.CallBackDatabase;
 import ch.epfl.sweng.radius.database.Database;
+import ch.epfl.sweng.radius.database.FirebaseUtility;
 import ch.epfl.sweng.radius.database.MLocation;
 import ch.epfl.sweng.radius.database.User;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(JUnit4.class)
+@PrepareForTest({ FirebaseAuth.class, FirebaseDatabase.class })
 public class PreferencesActivityTest {
 
     private PreferencesActivity test;
+    FirebaseAuth        mockedAuth = Mockito.mock(FirebaseAuth.class);
+    FirebaseUser mockedUser = Mockito.mock(FirebaseUser.class);
+    Task mockedTask = Mockito.mock(Task.class);
+
 
     @Before
     public void setUp() throws Exception {
@@ -23,6 +57,29 @@ public class PreferencesActivityTest {
     public void deleteUser() {
         PreferencesActivity.deleteUser();
         restoreCurrentUser();
+    }
+
+    @Test
+    public void setupPositiveButton()
+    {
+        PowerMockito.mockStatic(FirebaseAuth.class);
+        PowerMockito.mockStatic(FirebaseAuth.class);
+        when(FirebaseAuth.getInstance()).thenReturn(mockedAuth);
+        when(mockedAuth.getCurrentUser()).thenReturn(mockedUser);
+        when(mockedUser.getUid()).thenReturn("Coucou");
+        when(mockedTask.isSuccessful()).thenReturn(true);
+        when(mockedUser.delete()).thenReturn(mockedTask);
+
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                OnCompleteListener onCompleteListener = (OnCompleteListener) invocation.getArguments()[0];
+
+                onCompleteListener.onComplete(mockedTask);
+                return null;
+            }
+
+        }).when(mockedTask).addOnCompleteListener(any(OnCompleteListener.class));
     }
 
     private void restoreCurrentUser() {
