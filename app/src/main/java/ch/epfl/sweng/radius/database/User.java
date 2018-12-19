d4d5ccf52a0ff6ff89009b05f195156c818a1dce
Pinar Ayaz <pinarayaz123@gmail.com>
2018-12-19 11:17:20 +0100

    Refactor and comment User

--- app/src/main/java/ch/epfl/sweng/radius/database/User.java
+++ app/src/main/java/ch/epfl/sweng/radius/database/User.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * We can then store/access the states of each user in the database
  */
 public class User implements DatabaseObject, Serializable {
-    private static long idGenerator = 0;// Debugging purpose only
+    private static long idGenerator = 0; // Debugging purpose only
 
     private String userID;
     private Map<String, String> friendsRequests;
@@ -22,8 +22,10 @@ public class User implements DatabaseObject, Serializable {
     private Map<String, String> chatList = new HashMap<>();
     private Map<String, String> reportList;
 
-
-
+    /**
+     * Constructor to create new User object with the given user ID
+     * @param userID: String to set the userID as
+     */
     public User(String userID) {
         this.userID = userID;
         this.friendsRequests = new HashMap<>();
@@ -44,35 +46,65 @@ public class User implements DatabaseObject, Serializable {
         this(userID);
     }
 
-    // Getter
-
+    /**
+     * Getter for friends requests
+     * @return friendsRequests: HashMap that stores the friends requests of the user
+     */
     public Map<String, String> getFriendsRequests() {
         return friendsRequests;
     }
 
+    /**
+     * Getter for friends invitations
+     * @return friendsInvitations: HashMap that stores the friends invitations of the user
+     */
     public Map<String, String> getFriendsInvitations() {
         return friendsInvitations;
     }
 
+    /**
+     * Getter for friends
+     * @return friends: HashMap that stores the friends of the user
+     */
     public Map<String, String> getFriends() {
         return friends;
     }
-    // Setter
 
+    /**
+     * Getter for blocked users
+     * @return blockedUsers: ArrayList that stores the users blocked by this user
+     */
     public List<String> getBlockedUsers() {
         return blockedUsers;
     }
 
+    /**
+     * Setter for blocked users
+     * @param blockedUsers: ArrayList that stores the blocked users
+     */
     public void setBlockedUsers(List<String> blockedUsers) { this.blockedUsers = blockedUsers; }
 
+    /**
+     * Getter for chat list
+     * @return chatList: Map that stores the chats for this user
+     */
     public Map<String, String> getChatList() {
         return chatList;
     }
 
+    /**
+     * Setter for chat list
+     * @param chatList: Map that stores the chats
+     */
     public void setChatList(Map<String, String> chatList) {
         this.chatList = chatList;
     }
 
+    /**
+     * Returns the conversation id for a given userID
+     * @param userID: ID of the user to get the convID
+     * @return convID: ID of the conversation for the given userID
+     */
     public String getConvFromUser(String userID) {
         String convId = chatList.get(userID);
         if (convId == null) {
@@ -82,6 +114,11 @@ public class User implements DatabaseObject, Serializable {
         }
     }
 
+    /**
+     * Adds a friend request for the current user, if the user has sent a request to the friend as well,
+     * the friend is added to the friends of this user
+     * @param friend: the User to add to the friends requests
+     */
     public void addFriendRequest(User friend) {
         if (friendsInvitations.containsKey(friend.getID())) {
             friend.friendsRequests.remove(this.userID);
@@ -94,6 +131,10 @@ public class User implements DatabaseObject, Serializable {
         }
     }
 
+    /**
+     * Removes the given user from the friends of this user
+     * @param friend: The user to remove as friend
+     */
     public void removeFriend(User friend) {
         friends.remove(friend.getID());
         friend.friends.remove(getID());
@@ -102,9 +143,9 @@ public class User implements DatabaseObject, Serializable {
     }
 
     /**
-     * add a chat to a user
-     * @param otherUserId the other user ID
-     * @param chatID the chat ID
+     * Adds a chat to a user
+     * @param otherUserId: the other user ID
+     * @param chatID: the chat ID
      * @return the chat ID
      */
     public String addChat(String otherUserId, String chatID) {
@@ -114,6 +155,11 @@ public class User implements DatabaseObject, Serializable {
         return chatID;
     }
 
+    /**
+     * Create a chatlog with the given id and the id of this user
+     * @param otherUserId: The other user id to create a chat between
+     * @return: The newly created chat id
+     */
     public String newChat(String otherUserId) {
         ArrayList<String> ids = new ArrayList();
         ids.add(otherUserId);
@@ -123,22 +169,37 @@ public class User implements DatabaseObject, Serializable {
         return addChat(otherUserId, chatLogs.getID());
     }
 
+    /**
+     * Getter for this user's id
+     * @return userID: id of this user
+     */
     @Override
     public String getID() {
         return userID;
     }
 
+    /**
+     * Setter for user's id
+     * @param userID: String to set the user id as
+     */
     public void setID(String userID) {
         this.userID = userID;
     }
 
+    /**
+     * Adds new report to the reports list of this user
+     * @param reportingUserID: The id of the user that reported
+     * @param reportingReason: The reason for reporting
+     */
     public void addReport(String reportingUserID, String reportingReason) {
         reportList.put(reportingUserID, reportingReason);
     }
 
+    /**
+     * Getter for reports list of this user
+     * @return reportList: reports list of this user
+     */
     public Map<String, String> getReportList() {
         return reportList;
     }
-
-
 }
